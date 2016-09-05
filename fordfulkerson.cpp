9a68328ccc2d106260c7e04a38f36d9fbc8d9ad5
chunseok lee <chunseok.lee@samsung.com>
2016-09-05 09:38:15 +0900

    Ford-Fulkerson algorithm

---
+++ fordfulkerson.cpp
@@ -0,0 +1,25 @@
+/*
+  For personal practice. 
+  Original source code comes from ISBN 978-89-6626-054-6
+ */
+
+/*
+  Ford-Fulkerson's algorithm for Max Flow
+ */
+#include <iostream>
+
+
+using namespace std;
+
+const int INF=987654321;
+int V;
+
+// capacity[i][j] = capacity from i to j 
+// flow[i][j] = current flow from i to j
+
+
+int main(int argc, char *argv[])
+{
+  
+  return 0;
+}
